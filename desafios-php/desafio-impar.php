71a790484f0c950281b5923ef8597670e7acaa6a
Gustavo Alda <102753327+alda510@users.noreply.github.com>
2022-12-27 16:25:21 -0300

    Add files via upload

---
+++ desafios-php/desafio-impar.php
@@ -0,0 +1,7 @@
+<?php
+
+for ($contador = 1; $contador <100; $contador++) {
+    if ($contador % 2 != 0) {
+        echo $contador . PHP_EOL;
+    }
+}
---
+++ desafios-php/desafio-tabuada.php
@@ -0,0 +1,7 @@
+<?php
+
+$multiplicador = 3;
+
+for ($i = 1; $i < 10; $i++) {
+    echo "$multiplicador x $i = " . $multiplicador * $i . PHP_EOL;
+}
